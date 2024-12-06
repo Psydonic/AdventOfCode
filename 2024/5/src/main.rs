9c7e531dfa6e6b24a5770294f9eb84c7dd5b5b41
Ben Bellavia <benbellavia123@gmail.com>
2024-12-06 17:37:07 +0000

    loaded file

--- 2024/5/src/main.rs
+++ 2024/5/src/main.rs
@@ -1,3 +1,19 @@
+
+const FILENAME: &str = "resources/input.txt";
+
+// load the file and parse it
+fn parse_file(filename: &str) {
+    use std::fs;
+    
+    let content: String = fs::read_to_string(filename)
+        .expect("Error opening file");
+
+    println!("{}", content);
+}
+
 fn main() {
+    parse_file(FILENAME);
     println!("Hello, world!");
 }
+
+
