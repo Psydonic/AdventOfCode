3ab9b96229bbe6b5a4dca932a0eb48d442ab5449
Benjamin Bellavia <benbellavia123@gmail.com>
2024-12-14 16:06:07 +0000

    deleted target

--- 2024/10/target/debug/deps/hoof_it-8c790b815fb259db.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/storage/code/AdventOfCode/2024/10/target/debug/deps/hoof_it-8c790b815fb259db: src/main.rs
-
-/data/data/com.termux/files/home/storage/code/AdventOfCode/2024/10/target/debug/deps/hoof_it-8c790b815fb259db.d: src/main.rs
-
-src/main.rs:
--- 2024/10/target/debug/hoof_it.d
+++
@@ -1,1 +0,0 @@
-/data/data/com.termux/files/home/storage/code/AdventOfCode/2024/10/target/debug/hoof_it: /data/data/com.termux/files/home/storage/code/AdventOfCode/2024/10/src/main.rs
