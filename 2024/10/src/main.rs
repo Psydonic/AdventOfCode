413b505fd65f5af8dffbaf34685417aa7e44fcec
Benjamin Bellavia <benbellavia123@gmail.com>
2024-12-14 21:32:26 +0000

    Completed day 10

--- 2024/10/src/main.rs
+++ 2024/10/src/main.rs
@@ -1,160 +1,152 @@
-const FILENAME: &str = "resources/test.txt";
+use std::collections::HashSet;
+
+const FILENAME: &str = "resources/input.txt";
 
 fn parse_file(filename: &str) -> Vec<Vec<u32>> {
     use std::fs;
 
-    let content = fs::read_to_string(filename)
-        .expect("Error opening file");
+    let content = fs::read_to_string(filename).expect("Error opening file");
 
     return content
         .lines()
-        .map(|line| {
-            line.chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect()
-        })
+        .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
         .collect();
 }
 
-struct Trailhead {
-    start: Point,
-    paths: u32
-}
-
-impl Trailhead {
-    fn new(x: u16, y:  u16, grid: &Vec<Vec<u32>>) -> Self {
-        let p = Point{x, y};
-
-        // find all trails from point to a 9 in the grid
-        let trails:Vec<Trail> = find_trails(&p, 9, grid);
-
-        Self{start: p, trails: trails}
-    }
-
-}
-
-// find all directions from the given point with the given height
-fn find_trails(p: &Point, height: u8,  grid: &Vec<Vec<u32>>) -> Vec<Trail> {
-    if height == 1 {
-        let ones = vec![
-            p.above(), 
-            p.left(), 
-            p.below(grid.len() as u16), 
-            p.right(grid.get(0).unwrap().len() as u16)];
-
-        let ones: Vec<Point> = ones
-            .into_iter()
-            .filter_map(|p| p)
-            .collect();
-
-        return ones.into_iter().map(|p1| {
-            Trail{path: vec![p.clone(), p1]}
-        }).collect();
-    } else {
-        // find all trails to hieght -1
-        let sub_trails = find_trails(p, height - 1, grid);
-
-        // for each sub trails, see if there is any valid trails ahead
-        return step(trails, grid);
-    }
-}
-
-// given a set of trails, advance them, removinf if failed
-fn step(trails: Vec<Trail>, grid: &Vec<Vec<u32>>) -> Vec<Trail> {
-    for trail in trails {
-        let last = trail.path.last().expect("invalid starter trailhead");
-        let height: u32 = trail.path.len().try_into().unwrap();
-
-        // find the surrounding points with height
-        let surrounding = vec![
-            last.above(), 
-            last.left(), 
-            last.below(grid.len() as u16), 
-            last.right(grid.get(0).unwrap().len() as u16)];
-        let surrounding: Vec<Point> = surrounding
-            .into_iter()
-            .filter_map(|p| p)
-            .filter(|p| p.in(grid) == height)
-            .collect();
-    }
-
-    return vec![];
-}
-
-struct Trail {
-    path: Vec<Point>
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Point {
-    x: u16,
-    y: u16  
+    x: i16,
+    y: i16,
 }
 
 impl Point {
     // count number of paths from this point
-    fn count_paths(&self, grid: &Vec<Vec<u32>>, currentValue: u32) -> usize {
+    fn find_unique_destinations(&self, grid: &Vec<Vec<u32>>, current_value: u32) -> HashSet<Point> {
+        
+        // Base case if out of bounds
+        if !self.is_valid(grid) {
+            return HashSet::new();
+        }
+
+        // Base case if value is not current + 1
+        if self.at(grid) != current_value {
+            return HashSet::new();
+        }
+
+        // Base case if path is finished
+        if self.at(grid) == 9 {
+            return vec![self.clone()].into_iter().collect();
+        }
+
+        // Recuse on all neightbours
+        return self
+            .neighbours()
+            .iter()
+            .flat_map(|p| p.find_unique_destinations(grid, current_value + 1))
+            .collect();
+    }
+
+    fn count_unique_paths(&self, grid: &Vec<Vec<u32>>, current_value: u32) -> usize {
+        
         // Base case if out of bounds
         if !self.is_valid(grid) {
             return 0;
         }
 
         // Base case if value is not current + 1
-        if self.in(grid) != currentValue + 1:
+        if self.at(grid) != current_value {
             return 0;
         }
 
         // Base case if path is finished
-        if self.in(grid) == 9 {
+        if self.at(grid) == 9 {
             return 1;
         }
 
         // Recuse on all neightbours
-        return self.neighbours.count_paths(grid, self.in(grid)).sum();
+        return self
+            .neighbours()
+            .iter()
+            .map(|p| p.count_unique_paths(grid, current_value + 1))
+            .sum();
     }
-    
+
     fn is_valid(&self, grid: &Vec<Vec<u32>>) -> bool {
-        let w = grid.get(0).unwrap().len();
-        let h = grid.len();
-        return (self.x < 0 || self.y < 0 || self.x >= w || self.y >= h);
+        let w = grid.get(0).unwrap().len().try_into().unwrap();
+        let h = grid.len().try_into().unwrap();
+        return self.x >= 0 && self.y >= 0 && self.x < w && self.y < h;
     }
 
-    fn in(&self, grid: &Vec<Vec<u32>>) -> u32{
-        grid.get(self.y).unwrap(Vec<u32>).get(self.x).unwrap()
+    fn at(&self, grid: &Vec<Vec<u32>>) -> u32 {
+        return *grid
+            .get(self.y as usize)
+            .unwrap()
+            .get(self.x as usize)
+            .unwrap();
     }
 
-    fn neighbours(&self, limit: usize) {
-        let surroundings = vec![
-            Point{}
-        ]
+    fn neighbours(&self) -> Vec<Point> {
+        return vec![
+            Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+        ];
     }
 }
 
-fn find_starting_points(grid: &Vec<Vec<u32>>) -> Vec<Point> {
-    
-    // find the zeros
-    return grid.iter().enumerate().flat_map(|(y, row)| {
-        row.iter().enumerate().filter_map(move |(x, &n)| {
-            if n == 0 {
-                // build a trailhead here
-                Some(Point(
-                        x.try_into().unwrap(), 
-                        y.try_into().unwrap())
-            } else {
-                None
-            }
-        })
-    })
-    .collect();
-}
-
-fn main() { 
+fn main() {
     let grid = parse_file(FILENAME);
-    let startings_points = find_starting_points(&grid);
-    let count: usize = startings_points
+
+    let count: usize = grid
         .iter()
-        .map(|p| p.count_paths(&grid, 0))
+        .enumerate()
+        .map(|(y, row)| {
+            row
+                .iter()
+                .enumerate()
+                .map(|(x, _n)| {
+                    let p = Point {
+                            x: x.try_into().unwrap(),
+                            y: y.try_into().unwrap(),
+                        };
+                    p.find_unique_destinations(&grid, 0).len()
+                })
+                .sum::<usize>()
+        })
         .sum();
 
-    println!("Count: {}", count);
+    println!("Part 1 Count: {}", count);
+
+    let count: usize = grid
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row
+                .iter()
+                .enumerate()
+                .map(|(x, _n)| {
+                    let p = Point {
+                            x: x.try_into().unwrap(),
+                            y: y.try_into().unwrap(),
+                        };
+                    p.count_unique_paths(&grid, 0)
+                })
+                .sum::<usize>()
+        })
+        .sum();
+
+    println!("Part 2 Count: {}", count);
 }
