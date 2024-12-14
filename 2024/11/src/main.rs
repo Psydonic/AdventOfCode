bdd3497cb5219c30205feb65ec6f35405c016a98
Benjamin Bellavia <benbellavia123@gmail.com>
2024-12-14 21:49:45 +0000

    day 11 parsed file

--- 2024/11/src/main.rs
+++ 2024/11/src/main.rs
@@ -1,3 +1,19 @@
+const FILENAME: &str = "resources/test.txt";
+
+fn parse_file(filename: &str) -> Vec<u32> {
+    use std::fs;
+
+    let content = fs::read_to_string(filename)
+        .expect("failed to open file");
+
+    content
+        .split(" ")
+        .into_iter()
+        .map(|num| num.parse().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stones = parse_file(FILENAME);
+    println!("{:?}", stones);
 }
