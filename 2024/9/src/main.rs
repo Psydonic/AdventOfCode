01e366d2e462e2c6e96bcb411e7ea41f43d421d1
Ben Bellavia <benbellavia123@gmail.com>
2024-12-27 12:55:29 +0000

    updated day 9

--- 2024/9/src/main.rs
+++ 2024/9/src/main.rs
@@ -1,79 +1,106 @@
 const FILENAME: &str = "resources/test.txt";
 
+// A memory block has a size and an id
+// id is none if the block is empty
 #[derive(Debug, Clone)]
 struct Block {
-    size: u32,
-    id: Option<usize>,
+    pub size: u32,
+    pub id: Option<usize>,
 }
 
+// a disk is made up of a list of blocks
+// some are full and some are empty
 #[derive(Debug, Clone)]
 struct Disk {
     blocks: Vec<Block>,
 }
 
 impl Disk {
+    // construct a disk from a file
+    fn new(filename: &str) -> Self {
+        use std::fs;
+        let content = fs::read_to_string(filename).expect("error reading file");
+
+        let blocks = content
+            .chars()
+            .into_iter()
+            .enumerate()
+            .map(|(i, c)| {
+                let digit = c.to_digit(10).unwrap();
+                let id = if i % 2 == 0 { Some(i / 2) } else { None };
+                return Block { size: digit, id };
+            })
+            .collect();
+
+        Disk { blocks }
+    }
+
+
     // Compress the disk by moving the rightmost filled blocks to the leftmost free space,
     // splitting them if necessary.
     fn compress(&self) -> Disk {
-        let mut compressed_blocks: Vec<Block> = Vec::new();
-        let mut current_free_space: u32 = 0;
-        let mut filled_blocks: Vec<Block> = Vec::new();
+        // create a new disk to hold the compressed version
+        // i is our counter iterating forward, looking for empty blocks
+        // j is our counter iterating backwards looking for full blocks
+        // stop when i == j
+       
+        let mut compressed_blocks = vec![];
+        let mut i = 0;
+        let mut j = self.blocks.len() - 1;
 
-        // Separate filled and empty blocks
-        for block in &self.blocks {
-            match block.id {
-                Some(_) => filled_blocks.push(block.clone()),
-                None => current_free_space += block.size,
-            }
-        }
-
-        // Process filled blocks from rightmost to leftmost
-        while let Some(block) = filled_blocks.pop() {
-            if current_free_space > 0 {
-                if current_free_space > block.size {
-                    // Free space larger than filled block
-                    compressed_blocks.insert(0, Block {
-                        size: block.size,
-                        id: block.id,
-                    });
-                    current_free_space -= block.size;
-                } else if current_free_space < block.size {
-                    // Free space smaller than filled block
-                    compressed_blocks.insert(0, Block {
-                        size: current_free_space,
-                        id: block.id,
-                    });
-                    compressed_blocks.insert(0, Block {
-                        size: block.size - current_free_space,
-                        id: block.id,
-                    });
-                    current_free_space = 0;
-                } else {
-                    // Free space equal to filled block
-                    compressed_blocks.insert(0, Block {
-                        size: block.size,
-                        id: block.id,
-                    });
-                    current_free_space = 0;
-                }
+        while i < j {
+            // if the i block is full
+            if self.blocks[i].id.is_some() {
+                // move it to the compressed blocks list and increment
+                compressed_blocks.push(self.blocks.get(i).unwrap().clone());    
+                i += 1;
             } else {
-                compressed_blocks.insert(0, block);
+                // i block is empty
+                // if the j block is full
+                if self.blocks[j].id.is_some() {
+                    // if the space is larger than the block
+                    if self.blocks[i].size > self.blocks[j].size {
+                        // copy the block in
+                        compressed_blocks.push(self.blocks[j].clone());
+                        // copy a shrunk empty block in after
+                        let shrunk_block = Block { 
+                            size: self.blocks.get(i).unwrap().size - self.blocks.get(j).unwrap().size, 
+                            id: None
+                        };
+                        compressed_blocks.push(shrunk_block);
+
+                        // move both i and j
+                        i += 1;
+                        j -= 1;
+                    // if the space is the same size as the block
+                    } else if self.blocks[i].size == self.blocks[j].size {
+                        // copy full block to the empty space
+                        compressed_blocks.push(self.blocks[j].clone());
+                        
+                        // move  both i and j
+                        i += 1;
+                        j += 1;
+                    // if the space is smaller than the block size
+                    } else {
+                        // calculate how much free space we have from 
+                        // end of i to start of j
+                        // TODO
+                        
+                        // iterate frm i to j, filling in space
+                        // until the block is used up
+                        // TODO
+                    }
+                } else {
+                    // j block is empty, decrement
+                    j -= 1;
+                }
             }
         }
 
-        // Append any remaining free space at the end
-        if current_free_space > 0 {
-            compressed_blocks.push(Block {
-                size: current_free_space,
-                id: None,
-            });
-        }
-
-        Disk {
-            blocks: compressed_blocks,
-        }
+        Disk {blocks: compressed_blocks}
     }
-
+    
+    // calculate the checksum of the disk
     fn checksum(&self) -> usize {
         let mut counter: usize = 0;
         let mut checksum: usize = 0;
@@ -91,26 +118,8 @@ impl Disk {
     }
 }
 
-fn parse_file(filename: &str) -> Disk {
-    use std::fs;
-    let content = fs::read_to_string(filename).expect("error reading file");
-
-    let blocks = content
-        .chars()
-        .into_iter()
-        .enumerate()
-        .map(|(i, c)| {
-            let digit = c.to_digit(10).unwrap();
-            let id = if i % 2 == 0 { Some(i / 2) } else { None };
-            return Block { size: digit, id };
-        })
-        .collect();
-
-    Disk { blocks }
-}
-
 fn main() {
-    let disk = parse_file(FILENAME);
+    let disk = Disk::new(FILENAME);
     let compressed_disk = disk.compress();
     let checksum = compressed_disk.checksum();
     println!("{:?}", checksum);
