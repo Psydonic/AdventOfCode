1e233a8b4dd0cc8ad36a3534e5d34aa7f7e1326d
= <=>
2025-12-06 14:36:17 +0000

    Completed day 1

--- 2025/1/solution.java
+++ 2025/1/solution.java
@@ -1,3 +1,20 @@
-void main() {
-    System.out.println("hello world");
+import java.io.IOException;
+
+final String INPUT_FILE = "./input.txt";
+int total = 50;
+int count = 0;
+
+void main() throws IOException {    
+    
+    Files.lines(Path.of(INPUT_FILE))
+        .forEach((line) -> {
+            int number = Integer.valueOf(line.substring(1));    // Extract the number
+            count += (number / 100);                            // for every 100 we know it must go past 0 once
+            number %= 100;                                      // Find the remainder
+            int newTotal = total + (line.charAt(0) == 'L' ? -number : number);  // Rotate the dial
+            count += total != 0 && (newTotal <= 0 || newTotal >= 100) ? 1 : 0;  // Check if it went past zero
+            total = Math.floorMod(newTotal, 100);                               // Assign the new total
+        });
+    
+    System.out.println(count);
 }
